e00510f68fd5476eaaff3b220fc108d5f3c55b1e
ahmed alhennawi <ahmedalhennawi@gmail.com>
2025-11-05 22:37:46 +0100

    Initial commit

---
+++ exm4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace exm4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
